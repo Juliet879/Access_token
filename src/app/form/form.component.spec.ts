9d3753254d0ac0d72741a4c7360c387cc4ef33f0
JulietK <julietgisemba000@>
2020-08-31 18:27:08 +0300

    styled the form

--- src/app/form/form.component.spec.ts
+++ src/app/form/form.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NotFoundComponent } from './not-found.component';
+import { TokenFormComponent } from './form.component';
 
-describe('NotFoundComponent', () => {
-  let component: NotFoundComponent;
-  let fixture: ComponentFixture<NotFoundComponent>;
+describe('TokenFormComponent', () => {
+  let component: TokenFormComponent;
+  let fixture: ComponentFixture<TokenFormComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NotFoundComponent ]
+      declarations: [ TokenFormComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NotFoundComponent);
+    fixture = TestBed.createComponent(TokenFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
