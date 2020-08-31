4813a3703ecc088423460640203c15027e27042f
JulietK <julietgisemba000@>
2020-08-31 19:59:00 +0300

    deleted some files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,8 +19,9 @@ import { FormComponent } from './form/form.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     FormsModule,
-    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/repository-class/repository.spec.ts
@@ -0,0 +1,7 @@
+import { Repository } from './repository';
+
+describe('Repo', () => {
+  it('should create an instance', () => {
+    expect(new Repository()).toBeTruthy();
+  });
+});
